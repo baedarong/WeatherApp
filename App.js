cc37cbe6dc6c48fee7d8ffee4b3c3ab993153176
dahyeon.bae <dahyeon.bae@karfarm.com>
2021-11-08 15:54:47 +0900

    using weather API

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 
+const API_KEY = "ddc6afdeba4b52bfb507a513655a385b";
+// api key를 앱에 적어두면 안되는데 우선 하쟈 ~ 무료이긴 함
 const SCREEN_WIDTH = Dimensions.get("window").width;
 export default function App() {
   const [city, setCity] = useState("Loading");
-  const [location, setLocation] = useState();
+  const [days, setdays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWhether = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false); // user not granted
@@ -27,9 +36,14 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].street); // Array - Object - city
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setdays(json.daily);
   };
   useEffect(() => {
-    ask();
+    getWhether();
   }, []);
 
   return (
@@ -43,18 +57,21 @@ export default function App() {
         horizontal
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>SUNNY</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>SUNNY</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>SUNNY</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator size="large" color="black" />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>
+                {parseFloat(day.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.desc}>{day.weather[0].main}</Text>
+              <Text style={styles.tinytext}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
       <StatusBar />
     </View>
@@ -66,7 +83,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#f5f5dc",
   },
   city: {
-    flex: 1,
+    flex: 0.5,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -75,12 +92,13 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: "500",
   },
-  weather: {},
   day: {
     width: SCREEN_WIDTH,
     justifyContent: "center",
     alignItems: "center",
   },
-  temp: { fontSize: 178 },
+  weather: {},
+  temp: { fontSize: 127, fontWeight: "600", marginTop: -150},
   desc: { fontSize: 60 },
+  tinytext: { fontSize: 21 },
 });
